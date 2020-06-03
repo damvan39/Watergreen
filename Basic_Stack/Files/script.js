5ca132447d4f88aad1007f42eafe3dab07e746cf
Damian <damian.vdm52@gmail.com>
2020-06-03 17:19:54 +1200

    working database retrival

--- Basic_Stack/Files/script.js
+++ Basic_Stack/Files/script.js
@@ -9,7 +9,36 @@ function loadDoc(url, cFunction) {
     xhttp.open("GET", url, true);
     xhttp.send();
   }
-  function myFunction(xhttp) {
-    document.getElementById("demo").innerHTML =
-    xhttp.responseText;
+  function setData(url, id) {
+    var xhttp;
+    xhttp=new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        xhttpr = this
+        identifier_type = id[0]
+        identifier_index = id[1]
+        identifier_data = id[2]
+        
+        data = function() {
+          x = JSON.parse(xhttp.responseText);
+          x = x[identifier_type]
+          x = x[identifier_index]
+          x = x[identifier_data]
+          return x
+        } 
+
+        document.getElementById(identifier_type+identifier_index).innerHTML = data()
+        console.log(data["watert[0].data"])
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+
+  }
+  function mainInit() {
+    setData("/api/buffer", ["watert",0,"data"])
+    setData("/api/buffer", ["watert",1,"data"])
+    setData("/api/buffer", ["airt",0,"data"])
+    setData("/api/buffer", ["airh",0,"data"])
+
   }
--- Basic_Stack/server.js
+++ Basic_Stack/server.js
@@ -7,47 +7,21 @@ var Schema = mongoose.Schema;
 var dataSchema = new Schema({
 
   time: Date, // String is shorthand for {type: String}
-  airt: [{ data: Number, index: Number }],
-  airh: [{ data: Number, index: Number }],
-  watert: [{ data: Number, index: Number }],
+  airt: [{ data: Number }],
+  airh: [{ data: Number }],
+  watert: [{ data: Number }],
 },{collection :'data_buffer'});
 var data = mongoose.model('' , dataSchema)
 
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
-var testwrite = new data({
-  
-  time: 27/04/2020,
-  airt: [{data: 27.012, index:1}],
-  airh: [{data: 34.34, index:1}],
-  watert: [{data: 27.12, index:1}],
-
-})
-testwrite.save(function (err){
-console.log("entered function")
-
-  if (err) return handleError(err);
-
-});
-
-var query = data.find({ 'type': 'example1'})
-query.select('airt airh')
-query.limit(10)
-query.sort({ created_at: -1 });
-
-query.exec(function(err, data){
-
-  if (err) return handleError(err);
-  console.log(data)
-})
-
-
-app.route('/api').get(function(req, res)  {
+app.route('/api/buffer').get(function(req, res)  {
 
   console.log("api request recived, type: " + req.query.type)
+  
+  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
   var query = data.find({})
   query.select('')
@@ -61,6 +35,6 @@ app.route('/api').get(function(req, res)  {
   })
 
 });
-app.use(express.static('Files'))
+app.use('/', express.static('Files'))
 
 var server = app.listen(8080, function() {});
