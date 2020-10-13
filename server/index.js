831bbee85fdf9080453c178ed8579bc6e27f8c09
Damian <damian.vdm52@gmail.com>
2020-10-13 17:05:14 +1300

    simple graphql server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+const typeDefs = `
+type Query {
+    info: String!
+}
+`
+
+const resolvers = {
+    Query: {
+        info: () => `Ni Hao`
+    }
+}
+
+const server = new GraphQLServer ({
+    typeDefs,
+    resolvers,
+})
+server.start(() => console.log('server running'))
